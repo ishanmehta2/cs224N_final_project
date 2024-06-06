c1770cdd70d5474ed07e1789af02e77318ee5e38
ishanmehta2 <121464699+ishanmehta2@users.noreply.github.com>
2024-06-06 15:27:26 -0700

    Update GPT2_baseline.py

--- gpt2_baseline/GPT2_baseline.py
+++ gpt2_baseline/GPT2_baseline.py
@@ -24,7 +24,6 @@ for col in relevant_cols:
 with open('training_data.txt', 'w', encoding='utf-8') as file:
     file.write(training_text)
 
-# Load tokenizer and model
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
@@ -40,25 +39,19 @@ data_collator = DataCollatorForLanguageModeling(
 
 from transformers import pipeline
 
-
-# Initialize the generator with the fine-tuned model and standard tokenizer
 generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
-# Generate text
+# text generation
 text_prompt = ""
 generated_text = generator(text_prompt, max_length=100, num_return_sequences=1)
 print(generated_text[0]['generated_text'])
 
 def answer_crossword(clue, length):
-    # Format the prompt as expected by the model, e.g., "Clue: <clue> Answer:"
     prompt = f"Length of the word is: {length} Clue: {clue} Answer:"
 
     # Use the generator to generate text. Adjust max_length based on expected answer length
     response = generator(prompt, max_length=50, num_return_sequences=1)
     full_response = response[0]['generated_text']
-
-    # Optional: Process the response to extract the answer part
-    # This assumes the model's output format is consistent and includes the answer following "Answer:"
     answer = full_response.split("Answer:")[1].strip().split('\n')[0].strip()
 
     return answer
